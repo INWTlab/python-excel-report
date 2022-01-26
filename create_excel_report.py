1ec7695cefa2d8b624f144da1768486f7f692986
Mira Céline Klein <mira.klein@inwt-statistics.de>
2022-01-26 17:12:22 +0100

    #7 Text wrap (linebreaks) in script - not the final solution.

--- create_excel_report.py
+++ create_excel_report.py
@@ -10,8 +10,11 @@ CONFIG = {
         "font": "Open Sans",
         "size": "11",
         "font_color": "#2b2b2b",
+        "valign": "top",
     },
-    "column_widths": {0: 6, 1: 16, 2: 12, 3: 16},
+    "column_widths": {0: 6, 1: 12, 2: 12, 3: 16},
+    "column_heights": {0: 15, 1: 15, 2: 30, 3: 15},
+    "columns_linebreaks": [1],
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -42,12 +45,21 @@ column_header_format = workbook.add_format(CONFIG["column_header_format"])
 for col_num, col_name in enumerate(data.columns.values):
     sheet_report.write(0, col_num, col_name, column_header_format)
 
+# Allow linebreaks in predefined columns
+text_wrap_format = CONFIG["text_format"].copy()
+text_wrap_format["text_wrap"] = "True"
+text_wrap_format = workbook.add_format(text_wrap_format)
+for col_number in CONFIG["columns_linebreaks"]:
+    sheet_report.set_column(first_col=col_number, last_col=col_number, cell_format=text_wrap_format)
+
 # Freeze header row
 sheet_report.freeze_panes(row=1, col=0)
 
-# Set column widths
+# Set column widths and heights
 for col_index, width in CONFIG["column_widths"].items():
     sheet_report.set_column(col_index, col_index, width)
+for row_index, height in CONFIG["column_heights"].items():
+    sheet_report.set_row(row_index, height)
 
 # Save file
 writer.save()
